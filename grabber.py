bd78a35a08f771b5b2979ca98e9494fdd393086b
Crossed27 <100124773+Crossed27@users.noreply.github.com>
2022-02-23 18:04:21 +0000

    Update grabber.py

--- grabber.py
+++ grabber.py
@@ -6,6 +6,30 @@ print("""\
                           ██║   ██║██╔══██╗██╔══██║██╔══██╗██╔══██╗██╔══╝  ██╔══██╗
                           ╚██████╔╝██║  ██║██║  ██║██████╔╝██████╔╝███████╗██║  ██║
                            ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═════╝ ╚═════╝ ╚══════╝╚═╝  ╚═╝
+                                   Type A Number To Choose An Option
                                                                                                          
 """)
+
+shitList = [
+    "[1] Grab Token From Link",
+    "[2] Coming Soon",
+    "[3] Coming Soon"
+]
+
+for i in shitList:
+    print(i)
+
+strInput = input()
+
+# define a function
+def shitFunction():
+    print("W.I.P") # change this function's behaviour to whatever you want :]
+    input() # forgor
+
+# change number to whatever you like
+if strInput == "1":
+    shitFunction()
+
+# test it out ig
+
 input()
